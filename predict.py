f62b05fbddb6bb495851c7fe7c9ea61df19be80c
Pogayo <pogayo@alustudent.com>
2021-04-28 04:39:49 +0300

    deleted models - using an hugging face api

--- predict.py
+++ predict.py
@@ -1,46 +1,56 @@
-import torch
-from transformers import AutoTokenizer, AutoModelWithLMHead, AutoModelForSeq2SeqLM
-import numpy as np
+# import torch
+# from transformers import AutoTokenizer, AutoModelWithLMHead, AutoModelForSeq2SeqLM
+# import numpy as np
 import predict_luo
+import predict_acholi
+import predict_adhola
 
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelWithLMHead
+# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelWithLMHead
 
 
-device = 'cpu'
-if torch.cuda.is_available(): #check if GPU device is available
-    device = 'cuda' # assign the gpu to the device
+# device = 'cpu'
+# if torch.cuda.is_available(): #check if GPU device is available
+#     device = 'cuda' # assign the gpu to the device
 
-model_en_ach= AutoModelForSeq2SeqLM.from_pretrained("models/en-ach-model")
-tokenizer_en_ach = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-luo", use_fast=False)
+# model_en_ach= AutoModelForSeq2SeqLM.from_pretrained("models/en-ach-model")
+# tokenizer_en_ach = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-luo", use_fast=False)
 
-model_ach_en= AutoModelForSeq2SeqLM.from_pretrained("models/ach-en-model")
-tokenizer_ach_en = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-luo-en", use_fast=False)
+# model_ach_en= AutoModelForSeq2SeqLM.from_pretrained("models/ach-en-model")
+# tokenizer_ach_en = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-luo-en", use_fast=False)
 
-model_adh_en = AutoModelForSeq2SeqLM.from_pretrained("models/adh-en-model")
-tokenizer_adh_en = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-luo-en", use_fast=False)
+# model_adh_en = AutoModelForSeq2SeqLM.from_pretrained("models/adh-en-model")
+# tokenizer_adh_en = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-luo-en", use_fast=False)
 
-model_en_adh = AutoModelForSeq2SeqLM.from_pretrained("models/en-adh-model")
-tokenizer_en_adh = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-luo", use_fast=False)
+# model_en_adh = AutoModelForSeq2SeqLM.from_pretrained("models/en-adh-model")
+# tokenizer_en_adh = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-luo", use_fast=False)
 
 
 
 def return_results(data, source, target):
 
 	if source=="en" and target=="ach":
-		model = model_en_ach
-		tokenizer = tokenizer_en_ach
+		# model = model_en_ach
+		# tokenizer = tokenizer_en_ach
+		return predict_acholi.to_ach(data)
+
 
 	elif source=="ach" and target=="en":
-		model = model_ach_en
-		tokenizer = tokenizer_ach_en
+		# model = model_ach_en
+		# tokenizer = tokenizer_ach_en
+		return predict_acholi.to_en(data)
+
 
 	elif source=="adh" and target=="en":
-		model = model_adh_en
-		tokenizer = tokenizer_adh_en
+		# model = model_adh_en
+		# tokenizer = tokenizer_adh_en
+		return predict_adhola.to_en(data)
+
 
 	elif source=="en" and target=="adh":
-		model = model_en_adh
-		tokenizer = tokenizer_en_adh
+		# model = model_en_adh
+		# tokenizer = tokenizer_en_adh
+		return predict_adhola.to_adh(data)
+
 
 	elif source=="luo" and target=="en":
 		return predict_luo.to_en(data)
@@ -49,15 +59,15 @@ def return_results(data, source, target):
 		return predict_luo.to_luo(data)
 
 	else:
-		return "Sorry, something right did not happen"
+		return "Please select a valid combination. \nEnglish - Luo, \nEnglish - Acholi, \nEnglish - Dhopadola, \nLuo - English, \nAcholi - English, \n Dhopadhola - English\n"
 		
 
-	tokens=tokenizer.prepare_seq2seq_batch([data],padding=True,truncation=True, return_tensors="pt" )
+	# tokens=tokenizer.prepare_seq2seq_batch([data],padding=True,truncation=True, return_tensors="pt" )
 
-	tokens.to(device)
-	model.to(device)
-	translated = model.generate(**tokens)
-	tgt_text = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
-	result=tgt_text[0]
+	# tokens.to(device)
+	# model.to(device)
+	# translated = model.generate(**tokens)
+	# tgt_text = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
+	# result=tgt_text[0]
 
-	return result
+	# return result
--- predict_acholi.py
+++ predict_acholi.py
@@ -3,11 +3,10 @@ import requests
 import time
 from time import sleep
 
-
 # API_URL = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-luo"
 headers = {"Authorization": f"Bearer api_sRfRskxxxNmTHoMGhTrdMRsXRYjqjdKEsF"}
 
-luo_attempts=0
+ach_attempts=0
 en_attempts=0
 
 def query(payload, API_URL):
@@ -23,25 +22,25 @@ def run_query(text, API_URL, attempts):
 
         }
 	res = query(inputs, API_URL)
-	if res and len(res)> 0:
-		if res[0].get('translation_text'):
+	
+	if res and len(res)>0:
+		if res[0].get('generated_text'):
 			attempts=0 #return it to zero for the next request
-			return res[0].get('translation_text')
+			return res[0].get('generated_text')
 		else:
-			return str(res)
-			# sleep(20)
-			# attempts = attempts + 1
+			sleep(20)
+			attempts = attempts + 1
 	        
-			# if attempts < 4:
-			# 	run_query(inputs, API_URL, attempts)
-			# else:
-			# 	return "Attempts maxxed out. Please try again later"
+			if attempts < 4:
+				run_query(inputs, API_URL, attempts)
+			else:
+				return "Attempts maxxed out. Please try again later"
 
 
 def to_en(text):
-	return run_query(text, "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-luo-en", en_attempts)
+	return run_query(text, "https://api-inference.huggingface.co/models/Ogayo/mt-ach-en", en_attempts)
 	
 
 
-def to_luo(text):
-	return run_query(text, "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-luo", luo_attempts)
+def to_ach(text):
+	return run_query(text, "https://api-inference.huggingface.co/models/Ogayo/mt-en-ach", ach_attempts)
